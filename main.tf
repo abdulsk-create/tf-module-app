aa9948ed7ae5bf4ca0595d367fa5716fdd1c1d17
abdul <abdulsk054@gmail.com>
2023-09-03 10:57:56 +0530

    terraform

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_launch_template" "main" {
 
 resource "aws_autoscaling_group" "main" {
   name               = "${local.name_prefix}-asg"
-  vpc_identifier     = var.subnet_ids
+  vpc_zone_identifier     = var.subnet_ids
   desired_capacity   = var.desired_capacity
   max_size           = var.max_size
   min_size           = var.min_size
