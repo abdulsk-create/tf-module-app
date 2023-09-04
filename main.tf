10450ac2cceda648dc0b2074ad5730ef2b8c8404
abdul <abdulsk054@gmail.com>
2023-09-04 19:39:02 +0530

    terraform

--- main.tf
+++ main.tf
@@ -107,7 +107,7 @@ resource "aws_lb_listener_rule" "main" {
 #  protocol    = "HTTP"
 #  vpc_id      = var.default_vpc_id
 #}
-#
+
 #resource "aws_lb_target_group_attachment" "public" {
 #  count             = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb.addrs)) : 0
 #  target_group_arn  = aws_lb_target_group.public[0].arn
