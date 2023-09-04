fbed64afe0639c91c8ddcf526556d8140ff2e9f1
abdul <abdulsk054@gmail.com>
2023-09-04 19:50:03 +0530

    terraform

--- main.tf
+++ main.tf
@@ -99,14 +99,14 @@ resource "aws_lb_listener_rule" "main" {
   }
 }
 
-#resource "aws_lb_target_group" "public" {
-#  count       = var.component == "frontend" ? 1 : 0
-#  name        = "${local.name_prefix}-public"
-#  port        = var.port
+resource "aws_lb_target_group" "public" {
+  count       = var.component == "frontend" ? 1 : 0
+  name        = "${local.name_prefix}-public"
+  port        = var.port
 #  target_type = "ip"
-#  protocol    = "HTTP"
-#  vpc_id      = var.default_vpc_id
-#}
+  protocol    = "HTTP"
+  vpc_id      = var.default_vpc_id
+}
 
 #resource "aws_lb_target_group_attachment" "public" {
 #  count             = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb.addrs)) : 0
