38508fd0a4049d4bad0a2115ff28941e008ace21
abdul <abdulsk054@gmail.com>
2023-09-05 09:23:51 +0530

    terraform

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ resource "aws_route53_record" "main" {
   name    = var.component == "frontend" ? var.env : "${var.component}-${var.env}"
   type    = "CNAME"
   ttl     = 30
-  records = [ var.alb_name ]
+  records = [ var.private_alb_name ]
 }
 
 
@@ -85,7 +85,7 @@ resource "aws_lb_target_group" "main" {
 }
 
 resource "aws_lb_listener_rule" "main" {
-  listener_arn = var.listener
+  listener_arn = var.private_listener
   priority     = var.lb_priority
 
   action {
@@ -95,7 +95,7 @@ resource "aws_lb_listener_rule" "main" {
 
   condition {
     host_header {
-      values = ["${var.component}-${var.env}-entertanova.com"]
+      values = ["${var.component}-${var.env}.entertanova.com"]
     }
   }
 }
