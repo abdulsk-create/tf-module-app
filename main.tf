5de26a060d826bfd170a58f81c3cbb63747020b2
abdul <abdulsk054@gmail.com>
2023-09-13 11:01:46 +0530

    terraform

--- main.tf
+++ main.tf
@@ -89,6 +89,11 @@ resource "aws_autoscaling_group" "main" {
     value               = local.name_prefix
     propagate_at_launch = true
   }
+  tag {
+    key                 = "monitor"
+    value               = "yes"
+    propagate_at_launch = true
+  }
 }
 
 resource "aws_route53_record" "main" {
