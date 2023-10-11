32f965150a8112e56eb4dd94070eddcb0f87545c
abdul <abdulsk054@gmail.com>
2023-10-11 11:40:42 +0530

    Prod env

--- data.tf
+++ data.tf
@@ -10,7 +10,7 @@
 
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
+  name_regex       = "roboshop-ami-v1"
   owners           = ["219961291665"]
 }
 
