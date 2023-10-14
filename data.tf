7bda6d4fcad3459cca89205392204f1d6da7c69b
abdul <abdulsk054@gmail.com>
2023-10-14 09:31:47 +0530

    Prod env

--- data.tf
+++ data.tf
@@ -1,15 +1,15 @@
-data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
-}
-
-
 #data "aws_ami" "ami" {
 #  most_recent      = true
-#  name_regex       = "roboshop-v2"
-#  owners           = ["219961291665"]
+#  name_regex       = "Centos-8-DevOps-Practice"
+#  owners           = ["973714476881"]
 #}
+
+
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "roboshop-v2"
+  owners           = ["219961291665"]
+}
 
 data "dns_a_record_set" "private_alb" {
   host = var.private_alb_name
